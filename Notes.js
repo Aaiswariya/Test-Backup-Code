9490794a97f112307270d8ca9ab71984c6197d4f
Aaiswariya <44637513+Aaiswariya@users.noreply.github.com>
2025-11-05 22:31:26 +0530

    Add files via upload
    Initial Commit

---
+++ Notes.js
@@ -0,0 +1,8 @@
+` JSX --- Javascript Xml which allows the user to write HTML inside the Javascript file . It equates to the React.createElement() function .
+ When we write jsx code then the browser cannot understand it directly . To make it understandable we need to transpile it using tools 
+ like Babel.
+ It also written inside a single parent element . If we want to write multiple elements then we need to wrap them
+ inside a single parent element like a div or a fragment.`
+
+
+ 
